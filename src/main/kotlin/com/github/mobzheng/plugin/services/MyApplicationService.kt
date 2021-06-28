0936ffef16e46b203464d3893748a848c1b98eef
GitHub Action <action@github.com>
2021-06-28 12:23:15 +0000

    Template cleanup

--- src/main/kotlin/com/github/mobzheng/plugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/mobzheng/plugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.mobzheng.plugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.mobzheng.plugin.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/mobzheng/plugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/mobzheng/plugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.mobzheng.plugin.services
 
+import com.github.mobzheng.plugin.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
